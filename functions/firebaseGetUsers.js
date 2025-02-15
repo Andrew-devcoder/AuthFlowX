04a9a144a4c50be49518eaa4d9040f82c291bdf9
andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-02-15 14:57:02 +0100

    clr

--- functions/firebaseGetUsers.js
+++ functions/firebaseGetUsers.js
@@ -1,37 +1,3 @@
-// import { admin, checkFirebaseConnection } from './firebaseAdmin';
-// import { deleteUser } from './firebaseDelUser';
-
-// async function listAllUsers() {
-// 	try {
-// 		await checkFirebaseConnection();
-
-// 		let nextPageToken = undefined;
-// 		do {
-// 			const listUsersResult = await admin.auth().listUsers(1000, nextPageToken);
-
-// 			if (!listUsersResult.users || listUsersResult.users.length === 0) {
-// 				console.log('❗ Немає користувачів у Firebase.');
-// 				break;
-// 			}
-
-// 			listUsersResult.users.forEach((user) => {
-// 				const creationTime = new Date(user.metadata.creationTime);
-// 				console.log(creationTime.toDateString());
-
-// 				if (creationTime.toDateString() === 'Sat Feb 08 2025') {
-// 					deleteUser(user.uid);
-// 				}
-// 			});
-
-// 			nextPageToken = listUsersResult.pageToken;
-// 		} while (nextPageToken);
-// 	} catch (error) {
-// 		console.error('❌ Помилка отримання користувачів:', error);
-// 	}
-// }
-
-// export { listAllUsers };
-
 import { admin } from './firebaseAdmin.js';
 
 const getAllUsers = async () => {
